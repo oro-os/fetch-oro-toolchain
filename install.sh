ae9a92dd9c5508e595e9d6a77ba48e9b34754b8c
Josh Junon <junon@oro.sh>
2025-02-13 20:31:23 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function group {
+	echo "::group::$*"
+}
+
+function endgroup {
+	echo "::endgroup::"
+}
+
+function die {
+	echo "::error::$*"
+	exit 1	
+}
+
+group Check config and dependencies
+	if [ -z "${ORO_TOOLCHAIN_PATH-}" ]; then
+		die "ORO_TOOLCHAIN_PATH is not set"
+	fi
+	if [ ! -d "${ORO_TOOLCHAIN_PATH}" ]; then
+		die "directory not found: ${ORO_TOOLCHAIN_PATH}"
+	fi
+	echo "found toolchain at: ${ORO_TOOLCHAIN_PATH}"
+
+	export ORO_TOOLCHAIN_NAME="${ORO_TOOLCHAIN_NAME-oro-dev}"
+
+	rustup --version
+endgroup
+
+group Install toolchain as "+${ORO_TOOLCHAIN_NAME}"
+	rustup toolchain link "${ORO_TOOLCHAIN_NAME}" "${ORO_TOOLCHAIN_PATH}"
+endgroup
+
+group Check installation
+	rustc "+${ORO_TOOLCHAIN_NAME}" --version --verbose
+endgroup
